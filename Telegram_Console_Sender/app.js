dc92f1f9e2f1a190f9e1c811f25103a4b4fcfa31
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2022-11-14 21:06:40 +0200

    finish_TCS

--- Telegram_Console_Sender/app.js
+++ Telegram_Console_Sender/app.js
@@ -21,7 +21,7 @@ program
     "Send photo to Telegram Bot. Just drag and drop it console after sp-flag."
   )
   .alias("sp")
-  .argument("<patch>")
+  .argument("<path>")
   .action(function (input) {
     bot.sendPhoto(process.env.CHAT_ID, input).then(process.exit);
   });
