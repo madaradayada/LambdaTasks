a23cf88c9d66963b30f81d943c89c0e9ac217c31
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2022-11-15 15:00:26 +0200

    finish_TE

--- Telegram_Echo/app.js
+++ Telegram_Echo/app.js
@@ -1,38 +1,36 @@
 const TelegramBot = require("node-telegram-bot-api");
-const axios = require('axios');
-const fs = require('fs');
-require('dotenv').config();
+const axios = require("axios");
+const fs = require("fs");
+require("dotenv").config();
 
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
 process.env.NTBA_FIX_319 = 1;
 process.env.NTBA_FIX_350 = 0;
 
-console.log("bot succesfully started...")
+console.log("bot succesfully started...");
 
 bot.on("message", (msg) => {
-    const text = msg.text;
-    const chatId = msg.chat.id;
+  const text = msg.text;
+  const chatId = msg.chat.id;
+  const toLog =
+    text === "/photo"
+      ? `Пользователь ${msg.chat.username} запросил картинку`
+      : `Пользователь ${msg.chat.username} написал: ${text}`;
 
-    if (text !== '/photo') {
-      console.log(`Пользователь ${msg.chat.username} написал: ${text}`)
-    } else {
-      console.log(`Пользователь ${msg.chat.username} запросил картинку`)
-    }
-    
-    bot.sendMessage(chatId, `Вы написали: ${text}`)
-})
+  console.log(toLog);
 
-bot.onText(/\/photo/, msg => {
+  bot.sendMessage(chatId, `Вы написали: ${text}`);
+});
 
-    axios({
-        method: 'get',
-        url: 'https://picsum.photos/200/300',
-        responseType: 'stream'
-      })
-        .then(function (response) {
-          response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
-        });
+bot.onText(/\/photo/, (msg) => {
+  axios({
+    method: "get",
+    url: "https://picsum.photos/200/300",
+    responseType: "stream",
+  }).then(function (response) {
+    response.data.pipe(fs.createWriteStream("random_photo.jpg"));
+  });
 
-    bot.sendPhoto(msg.chat.id, fs.readFileSync('ada_lovelace.jpg'))
-})
+  bot.sendPhoto(msg.chat.id, fs.readFileSync("random_photo.jpg"));
+});
