d856147d9926836577b33b98775ed0ce47aeea22
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2023-12-02 00:15:39 +0200

    update_api

--- TelegramBot_Weather_Forecast/api.js
+++ TelegramBot_Weather_Forecast/api.js
@@ -1,12 +1,12 @@
 const axios = require("axios");
 require("dotenv").config();
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=Kiev,ua&appid=${process.env.OPENWEATHERMAPAPIKEY}&units=metric`;
+const url = `https://api.openweathermap.org/data/2.5/forecast?q=Kiev,ua&appid=${process.env.OPENWEATHERMAPAPIKEY}&units=metric`;
 
 function sendForcastMin() {
    axios.get(url)
    .then(response => {
-    console.log(response.data);
+    console.log(response.data.list);
    })
    .catch((error) => {
     console.error(error);
